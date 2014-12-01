7d1a88a4ccec8f527ce0af7372596aaaf34a7e1f
Max <maxim.gavrilenko@pdffiller.com>
2014-12-01 22:33:44 +0200

    Proxies block report

--- controllers/ProxyCronController.php
+++ controllers/ProxyCronController.php
@@ -15,6 +15,7 @@ class ProxyCronController extends Controller
 {
     
     private $_badProxies = [];
+    private $_countBadProxies = 0;
 
     public function actionBadProxy()
     {
@@ -25,7 +26,7 @@ class ProxyCronController extends Controller
         $this->checkProxies(ProxySpider::find()->all(), ProxySpider::tableName());
 
         \Yii::$app->mailer
-            ->compose('proxyBlockReport', ['count_total' => count($this->_badProxies), 'data' => $this->_badProxies])
+            ->compose('proxyBlockReport', ['count_total' => $this->_countBadProxies, 'data' => $this->_badProxies])
             ->setFrom('maxim.gavrilenko@pdffiller.com')
             ->setTo(['maxim.gavrilenko@pdffiller.com', 'koshevchenko@gmail.com'])
             ->setSubject('Proxy Not Worked')
@@ -55,6 +56,7 @@ class ProxyCronController extends Controller
             if(isset($res['info'])) {
                 if($res['info']['http_code'] == 0) {
                     $this->_badProxies[$table][] = $proxy;
+                    $this->_countBadProxies++;
                 }
             }
         }
