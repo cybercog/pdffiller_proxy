cfac9bc9b35cd7629ac19f550fbde968b521b866
Max <maxim.gavrilenko@pdffiller.com>
2014-12-01 17:51:36 +0200

    Proxy blocked done

--- controllers/ProxyCronController.php
+++ controllers/ProxyCronController.php
@@ -57,7 +57,7 @@ class ProxyCronController extends Controller
                     $this->_badProxies[$table][] = $proxy;
                 }
             }
-            
         }
+        
     }
 }
